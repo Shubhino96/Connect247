a2f3ee0dfb3605f0e44a4f38f7429cb5762ad5cc
Anurag3744 <anuragpandey3744@gmail.com>
2017-09-17 23:43:18 +0530

    New Commit

--- app/src/main/java/zerotoonecloud/connect247/HomePage.java
+++ app/src/main/java/zerotoonecloud/connect247/HomePage.java
@@ -3,11 +3,7 @@ package zerotoonecloud.connect247;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
+import android.text.TextUtils;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,12 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -39,6 +31,11 @@ public class HomePage extends AppCompatActivity
 
         ViewFlipper vf = (ViewFlipper) findViewById(R.id.flipper);
         vf.startFlipping();
+        TextView textView = (TextView) this.findViewById(R.id.general_contact);
+        textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        textView.setText("+91-XXXXXXXXX                                                                       +91-XXXXXXXXXXX");
+        textView.setSelected(true);
+        textView.setSingleLine(true);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
